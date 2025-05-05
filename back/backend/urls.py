b6f420b89aef6498c9fac96da5d5c47944b4b987
owens_lopez1 <owenslopez211@gmail.com>
2025-05-05 14:32:25 -0400

    actualizacion en urls de proyecto para eliminar conflicto con django admin

--- back/backend/urls.py
+++ back/backend/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('djadmin/', admin.site.urls),
     path('api/', include('authentication.urls')),
     path('api/profiles/', include('profiles.urls')),
     path('api/appointments/', include('appointments.urls')),
