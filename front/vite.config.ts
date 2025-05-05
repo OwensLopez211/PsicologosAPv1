e928b3daed93074fc4bafcc77b0e0512d8c1a52c
owens_lopez1 <owenslopez211@gmail.com>
2025-05-05 16:34:31 -0400

    actualizacion en urls de proyecto para eliminar conflicto con django admin

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,22 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// URL por defecto para la API
-const API_URL = 'https://186.64.113.186'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'import.meta.env.VITE_API_URL': JSON.stringify(API_URL),
-  },
   server: {
     host: true,
     port: 5173,
     allowedHosts: ['mentaliza.loca.lt', 'localhost'],
     proxy: {
       '/api': {
-        target: API_URL,
+        target: 'http://localhost:8000',
         changeOrigin: true,
         secure: false,
       }
